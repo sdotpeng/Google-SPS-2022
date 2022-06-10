7e979fc9edcdfeca8c79f455f53235ba7c20032e
Siyuan Peng <speng24@colby.edu>
2022-06-10 18:54:00 +0000

    Add form at the buttom of HTML

--- portfolio/src/main/java/com/google/sps/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/FormHandlerServlet.java
@@ -9,24 +9,29 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Get the value entered in the form.
-    String firstName = request.getParameter("first-name");
-    String lastName = request.getParameter("last-name");
-    String email = request.getParameter("email");
+        // Get the value entered in the form.
+        String firstName = request.getParameter("first-name");
+        String lastName = request.getParameter("last-name");
+        String email = request.getParameter("email");
 
-    String formType = request.getParameter("form-type");
+        String formType = request.getParameter("form-type");
 
-    String message = request.getParameter("message");
+        String message = request.getParameter("message");
 
-    // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: ");
+        // Print the value so you can see it in the server logs.
+        StringBuilder sb = new StringBuilder();
+        sb.append("--- New form ---");
+        sb.append("Type: " + formType + "\n");
+        sb.append("Name: " + firstName + " " + lastName + "\n");
+        sb.append("Email: " + email + "\n");
+        sb.append("Message: " + message);
 
-    response.getWriter().println(firstName + lastName + email + formType + message);
+        System.out.println(sb.toString());
 
-    // Write the value to the response so the user can see it.
-    response.getWriter().println("Contact form sent! Expect to hear back within one week :)");
-  }
+        // Write the value to the response so the user can see it.
+        response.getWriter().println("Contact form sent! Expect to hear back within one week :)");
+    }
 }
