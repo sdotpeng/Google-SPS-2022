f674f5f9d4892cf5215980e32c1aa28ab01e1ddb
Siyuan PENG <16830124+sdotpeng@users.noreply.github.com>
2022-06-21 12:46:03 -0400

    Update translate script

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['I can cook fried rice in more than 50 ways', 'I love reading cookbook, history, and detective fictions', 'My favorite programming language is Python', 'I won national prize for youth swimming.'];
+    ['I can cook fried rice in more than 50 ways', 'I love reading cookbook, history, and detective fictions', 'My favorite programming language is Python', 'I won national prize for youth swimming.'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -29,11 +29,12 @@ function addRandomGreeting() {
 
 
 function requestTranslation() {
-  const aboutme = document.getElementById('aboutme').value;
+  const aboutme = "My name is Siyuan, and I also go by Ricky or Rickesh (got it from my south asian friends). I am a rising junior double majoring in Computer Science and Mathematics at Colby College, Maine. Since I started programming in Pascal during middle school, I have found my passion in building and tinkering things with various technologies. I have been doing Eye Tracking in Programming and Biometrics research for the past two years, and I have some professional experience in Product Management and User Experience design. Recently in Google SPS, I started to learn web programming in Java. It is something that I always wanted to learn, and I want to master it in this summer.";
   const languageCode = document.getElementById('language').value;
 
   const resultContainer = document.getElementById('translation');
   resultContainer.innerText = 'Loading...';
+
 
   const params = new URLSearchParams();
   params.append('text', aboutme);
@@ -43,7 +44,7 @@ function requestTranslation() {
     method: 'POST',
     body: params
   }).then(response => response.text())
-  .then((translatedMessage) => {
-    resultContainer.innerText = translatedMessage;
-  });
+    .then((translatedMessage) => {
+      resultContainer.innerText = translatedMessage;
+    });
 }
